3411fe90db9b69e7128888da6386543afbdd91d0
David Stewart <stewartd18@students.ecu.edu>
2020-05-29 12:31:32 -0400

    Set up the recycler view viewholder and adapter

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
@@ -16,6 +17,7 @@ private const val TAG = "CrimeListFragment"
 class CrimeListFragment: Fragment() {
 
     private lateinit var crimeRecyclerView: RecyclerView
+    private var adapter: CrimeAdapter? = null
 
     private val crimeListViewModel: CrimeListViewModel by lazy{
         ViewModelProviders.of(this).get(crimeListViewModel::class.java)
@@ -34,10 +36,55 @@ class CrimeListFragment: Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
 
+        //get a reference to our recycler view and set up a layout manager
         crimeRecyclerView = view.findViewById(R.id.crime_recycle_view) as RecyclerView
-        crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+        crimeRecyclerView.layoutManager = LinearLayoutManager(context)//need a layout manager
 
        return view
+        updateUI()
+    }
+
+    //this is the proverbial 'first domino'
+    //when it is called the crime list is snagged from the view model and passed to
+    //an adapter through the constructor
+    //lastly the recyclerview adapter property is set to the adapter we just made
+    private fun updateUI(){
+        val crimes = crimeListViewModel.crimes
+        adapter = CrimeAdapter(crimes)
+        crimeRecyclerView.adapter = adapter
+    }
+
+    //A recycler view never creates views directly, it interacts with view holders
+    private inner class CrimeHolder(view: View):RecyclerView.ViewHolder(view){
+        val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
+        val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+    }
+
+    //need a crime adapter to create the viewHolders
+    private inner class CrimeAdapter(var crimes: List<Crime>):RecyclerView.Adapter<CrimeHolder>(){
+
+        //overriden adapter class method
+        //this method creates the view to display, wraps it in a view holder, and returns the result
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
+            val view = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
+            return CrimeHolder(view)
+        }
+
+        //overriden adapter class method to get the number of objects we have to produce
+        override fun getItemCount(): Int {
+            return crimes.size
+        }
+
+        //overridden adapter class method
+        //populates the given view holder with the object at the given postion
+        override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+            val crime = crimes[position]
+            holder.apply {
+                //asign the textviews the information from the crime object
+                titleTextView.text = crime.title
+                dateTextView.text = crime.date.toString()
+            }
+        }
     }
 
     companion object {
