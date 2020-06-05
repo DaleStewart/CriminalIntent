608ba1da65d6226472d5edb48cce4ba45daa85b4
David Stewart <stewartd18@students.ecu.edu>
2020-06-05 14:56:59 -0400

    Data base classes added and basic functions crated

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDao.kt
@@ -0,0 +1,15 @@
+package com.bignerdranch.android.criminalintent.database
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.bignerdranch.android.criminalintent.Crime
+import java.util.*
+
+@Dao
+interface CrimeDao {
+    @Query("SELECT * FROM crime")
+    fun getCrimes(): List<Crime>
+
+    @Query("SELECT *  FROM crime WHERE id = (:id)")
+    fun getCrime(id: UUID): Crime?
+}
