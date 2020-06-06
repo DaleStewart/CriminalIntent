bd8bc0b6ec5c9aca9666be3bd16445ad7c0f8a99
David Stewart <stewartd18@students.ecu.edu>
2020-06-06 15:31:25 -0400

    Enabled the menu to respond to new_crime being pressed and actualy add a new crime to the room database

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -42,6 +42,19 @@ class CrimeListFragment: Fragment() {
     }
     //----------------------------------------------------------------------------------------------
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.new_crime -> {
+                val crime = Crime()
+                crimeListViewModel.addCrime(crime)
+                callbacks?.onCrimeSelected(crime.id)
+                true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+    //----------------------------------------------------------------------------------------------
+
     //----------------------------------------------------------------------------------------------
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -5,6 +5,12 @@ import androidx.lifecycle.ViewModel
 class CrimeListViewModel : ViewModel() {
     private val crimeRepository = CrimeRepository.get()
     val crimeListLiveData = crimeRepository.getCrimes()
+
+    fun addCrime(crime: Crime){
+        crimeRepository.addCrime(crime)
+    }
+
+
     //----------------------------------------------------------------------------------------------
     /*val crimes = mutableListOf<Crime>()
 
