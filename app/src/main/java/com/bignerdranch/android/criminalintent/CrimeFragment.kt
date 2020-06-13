0e149ec08e5205d4523539cd5367a74f637d4247
David Stewart <stewartd18@students.ecu.edu>
2020-06-13 14:06:35 -0400

    Added content descriptions for talkback to function properly

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
@@ -232,13 +232,19 @@ class CrimeFragment  : Fragment(), DatePickerFragment.Callbacks{
         }
         updatePhotoView()
     }
+    //----------------------------------------------------------------------------------------------
 
+    //----------------------------------------------------------------------------------------------
     private fun updatePhotoView() {
         if (photoFile.exists()) {
             val bitmap = getScaledBitmap(photoFile.path, requireActivity())
             photoView.setImageBitmap(bitmap)
+            photoView.contentDescription =
+                getString(R.string.crime_photo_image_description)
         } else {
             photoView.setImageDrawable(null)
+            photoView.contentDescription =
+                getString(R.string.crime_photo_no_image_description)
         }
     }
     //----------------------------------------------------------------------------------------------
